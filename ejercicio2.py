97db8d7a9c8a173d0d69820d1decade4a2f9ebca
cecilia <cecilia_rmz@live.com>
2020-09-30 22:54:07 -0500

    ejercicio.py
    ejercicio 1 de definiciones de versiones

---
+++ ejercicio2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 22:32:39 2020
+
+@author: diegoodriozola
+"""
+
+def hello():
+    "prints hola"
+    print("hola")
+def area(radio):
+    "saca el area de un circulo"
+    area= 3.14*radio**2
+    print("el area del circulo es:", area)
+    
+def triangulo(b,h):
+    "saca el area del triangulo"
+    at=b*h*.5
+    print("el area del triangulo es", at)
